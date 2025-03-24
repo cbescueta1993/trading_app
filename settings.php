e548aa2d33edca395ba0eb45cffbc74ad49e7764
cbescueta <cbescueta@davao-water.gov.ph>
2025-03-24 14:11:39 +0800

    fix err

--- settings.php
+++ settings.php
@@ -42,7 +42,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="icon" type="image/x-icon" href="favicon.ico"></link>
 </head>
-<body>
+<body class="bg-light">
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="dashboard.php">Dashboard</a>
+        </div>
+    </nav>
+
+    
 <div class="container mt-5">
     <h3>Settings</h3>
 
