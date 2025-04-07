baabb9dd732d3b71e83256024166168c6bddeb1d
cbescueta <cbescueta@davao-water.gov.ph>
2025-04-07 11:09:45 +0800

    fix

--- redirect_alertlogs.php
+++ redirect_alertlogs.php
@@ -2,7 +2,7 @@
 $input = file_get_contents("php://input");
 file_put_contents("alertlog_okx.txt", $input . PHP_EOL, FILE_APPEND);
 
-$input='XRPUSDT;BUY';
+//$input='XRPUSDT;BUY';
 header("Location: insertalertlog.php?input=".$input);
 exit;
 ?>
