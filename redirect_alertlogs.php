5aca105d03f2bfd8366d70067463d3b97985cd18
cbescueta <cbescueta@davao-water.gov.ph>
2025-07-02 15:41:40 +0800

    log

--- redirect_alertlogs.php
+++ redirect_alertlogs.php
@@ -1,6 +1,6 @@
 <?php
 $input = file_get_contents("php://input");
-$input='WIF;BUY;07022025';
+//$input='WIF;BUY;07022025';
 file_put_contents('alertlog_okx.txt', $input . PHP_EOL, FILE_APPEND);
 exit;
 
